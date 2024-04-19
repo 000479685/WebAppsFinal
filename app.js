5d1e9033761082b97ecf5baec8fc882d3a78240c
Aiden Wu <wuazhihong@gmail.com>
2024-04-18 21:35:12 -0700

    testing if a get referring to index works

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.error(`Error connecting to the database: ${error}`);
 })
     
+app.get('/', function (req, res) {
+    res.render('index', {});
+  });
+
 app.use(express.static(path.join(__dirname, 'main')));
 
 app.use('/api/IntroWebAppsFinal/v1/users', UserRoutes);
